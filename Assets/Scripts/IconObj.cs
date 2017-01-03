41939c091e70afc998fb9bf8c07920801905aeab
Adlez <srgnt_booyaw@live.ca>
2017-01-02 22:14:42 -0500

    InitialCommit
    Includes Skeletal Structure for Items, Sending Items to and from the
    inventory, Data for Levels, Characters, and expeditions, structure of
    some core functions including going on an adventure and updating
    explorers on said adventure.
    Nothing displays yet.

---
+++ Assets/Scripts/IconObj.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IconObj : MonoBehaviour
+{
+    //public as necessary
+    protected Sprite io_IconBox;
+    protected Sprite io_IconImage;
+    protected Text io_Name;
+    protected GameObject io_ObjectForThisIcon;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/LevelScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelScript : MonoBehaviour
+{
+    //add "public" as necessary
+    public GameObject lv_GameController;
+    public GameObject lv_PartyObject;
+    string lv_Name;
+
+    int[] lv_ToolRequiredIndex;
+    int[] lv_LootIndex; //corresponds to treasure array, used to search for the right item
+    List<GameObject> lv_PotentialLootList = new List<GameObject>();
+
+    public float lv_Distance;
+    public bool lv_IsActive;
+
+    
+}
